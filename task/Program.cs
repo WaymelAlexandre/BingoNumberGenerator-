27f684793793aaa457c0a5d24944a5233f5f5e9b
alex w <103083020@student.swin.edu.au>
2020-06-03 18:49:41 +1000

    done for today i forgot to mush

--- task/Program.cs
+++ task/Program.cs
@@ -15,51 +15,46 @@ namespace task
             System.Console.WriteLine(" ");
             string blabla = Console.ReadLine();
             int LimitC = int.Parse(blabla);
-
             System.Console.WriteLine(" ");
             System.Console.WriteLine(" ");
 
             // player choose what he whant a do 
-
             System.Console.WriteLine("Welcom to the bingo club of swinburne");
             System.Console.WriteLine(" 1 Drawn the next number  ");
             System.Console.WriteLine(" 2 view all drawn number ");
             System.Console.WriteLine(" 3 Check specific number ");
             System.Console.WriteLine(" 4 Exit");
+
+            // choose inpute for the menu 
             string TheInputPlayer = Console.ReadLine();
             int ChooseNum = int.Parse(TheInputPlayer);
-
             ChooseNum = Convert.ToInt32(TheInputPlayer);
             System.Console.WriteLine();
             System.Console.WriteLine();
-
-
-            ///start  to work  
 
             //need to have a the limit ok the total number can be drawn
 
             //all number stock in list<> 
             List<int> listnum = new List<int>();
 
-            for (int i = 0; i <= LimitC; i++)
-            {
-                listnum.Add(i);
-            }
+
 
             //find solotion how to check if it is a numbre only
-            /*
-            if (LimitC.GetType() != typeof(int))
-                {
-                    System.Console.WriteLine("thats is not a num");
-                }
-            */
+            //choose 1
             if (ChooseNum == 1)
             {
+                //choose a generet random numbre 
                 Random r = new Random();
                 int RandomNum = r.Next(0, LimitC);
+                // remove the randomnumbre for the list 
+                listnum.Remove(RandomNum);
+
 
                 System.Console.WriteLine(RandomNum);
+
+                
             }
+            /*
             else if (ChooseNum == 2)
             {
                 System.Console.WriteLine(" drawen list ");
@@ -72,7 +67,11 @@ namespace task
             {
                 System.Console.WriteLine(" exit ");
             }
-            
+            else
+            {
+                System.Console.WriteLine("pls choose with the num 1 to 4 ");
+            }
+            */
 
 
 
