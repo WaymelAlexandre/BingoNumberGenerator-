53261b4a0a6477ef9b5b35230dfe79755cc3116d
alexandre <103083020@student.swin.edu.au>
2020-06-09 21:27:24 +1000

    one bug menue fix 'if erreur new value is valid know'

--- task/Program.cs
+++ task/Program.cs
@@ -10,7 +10,8 @@ namespace task
         {
 
             //--------------------------------------welcoming part and set up the Upper limit 
-
+            //ok need to fix No lettre sur le choise du menu
+            //frist choose if is it incorect mais ce second et bon is not working 
             System.Console.WriteLine("---------------------------------------------------------------------------------------------------");
             System.Console.WriteLine("---------------------------------------------------------------------------------------------------");
             System.Console.WriteLine("welcome let's play a bingo befor starting choose the upper limit  ");
@@ -18,58 +19,75 @@ namespace task
             System.Console.WriteLine("---------------------------------------------------------------------------------------------------");
             System.Console.WriteLine("---------------------------------------------------------------------------------------------------");
             string MaxNumbre = Console.ReadLine();
-            int MaxNum;
+            int MaxNum = int.Parse(MaxNumbre);
 
-            if (int.TryParse(MaxNumbre, out MaxNum))
+
+
+            //fix the limit of  20 to 999
+            if (MaxNum <= 20 || MaxNum > 1000)
             {
 
-                //fix the limit of  20 to 999
-                if (MaxNum <= 20 || MaxNum > 1000)
+                System.Console.WriteLine("------------------------------------------------------------");
+                System.Console.WriteLine("Have some limit ;) by the way mine is 999 and 20 drawns as minumun");
+                System.Console.WriteLine("------------------------------------------------------------");
+                for (int i = 0; i < 5; i++)
                 {
-                    System.Console.WriteLine("------------------------------------------------------------");
-                    System.Console.WriteLine("Have some limit ;) by the way mine is 999 and 20 drawns as minumun");
-                    System.Console.WriteLine("------------------------------------------------------------");
-                    for (int i = 0; i < 5; i++)
+
+                    MaxNumbre = Console.ReadLine();
+                    MaxNum = int.Parse(MaxNumbre);
+
+                    if (MaxNum! < 20 || MaxNum! > 1000)
                     {
-                        System.Console.WriteLine("try again");
-                        MaxNumbre = Console.ReadLine();
+
+                        System.Console.WriteLine("again");
                     }
-                    System.Console.WriteLine("--------------------------------------------------");
-                    System.Console.WriteLine("--------------------------------------------------");
-                    System.Console.WriteLine("you dont want a play faire enghout good by");
-                    System.Console.WriteLine("--------------------------------------------------");
-                    System.Console.WriteLine("--------------------------------------------------");
-                }
-                //the Numbre to use in array/list
-                else if (MaxNum >= 21)
-                {
-                    System.Console.WriteLine("You limit for this game is " + MaxNum + " know is time to ");
+                    else
+                    {
+                        System.Console.WriteLine("You limit for this game is " + MaxNum + " know is time to ");
+                        break;
+                    }
 
                 }
 
-                else
+
+            }
+            //else if (check fir the lettre thing here )
+            
+            
+            
+                
+            //the Numbre to use in array/list
+            else if (MaxNum >= 21)
+            {
+                System.Console.WriteLine("You limit for this game is " + MaxNum + " know is time to ");
+
+            }
+
+            else
+            {
+                System.Console.WriteLine("--------------------------------------------------");
+                System.Console.WriteLine("--------------------------------------------------");
+                System.Console.WriteLine(" || You have 5 try after you will simply exit the game || ");
+                System.Console.WriteLine("--------------------------------------------------");
+                System.Console.WriteLine("--------------------------------------------------");
+                for (int i = 0; i < 5; i++)
                 {
-                    System.Console.WriteLine("--------------------------------------------------");
-                    System.Console.WriteLine("--------------------------------------------------");
-                    System.Console.WriteLine(" || You have 5 try after you will simply exit the game || ");
-                    System.Console.WriteLine("--------------------------------------------------");
-                    System.Console.WriteLine("--------------------------------------------------");
-                    for (int i = 0; i < 5; i++)
-                    {
-                        System.Console.WriteLine("try again" + i++);
-                        MaxNumbre = Console.ReadLine();
-                    }
+                    System.Console.WriteLine("try again" + i++);
+                    MaxNumbre = Console.ReadLine();
                 }
             }
 
+
             //--------------------------------------Pick numbre form Menu()--------------------------------
+
+
 
             //----------------------------------if numbre one (random numbre)
             int MenuNumbre = Menu();
             for (int i = 0; i < MaxNum; i++)
             {
-                
-                
+
+
                 //No duplicate numbers should be drawn need to work on it 
 
                 if (MenuNumbre == 1)
@@ -82,24 +100,26 @@ namespace task
                     System.Console.WriteLine("the Drawn numbre is  " + RandomNum);
                     System.Console.WriteLine("--------------------------------------------------");
                     System.Console.WriteLine("--------------------------------------------------");
-  
+
 
                 }
 
-            //----------------------------------if numbre 2 (Numbre list) not started
+                //----------------------------------if numbre 2 (Numbre list) not started
 
                 else if (MenuNumbre == 2)
                 {
                     System.Console.WriteLine("not finish");
+                    break;
                 }
 
                 //----------------------------------if numbre3 prompted to enter numbers one by one to check if they have been drawn
                 else if (MenuNumbre == 3)
                 {
                     System.Console.WriteLine("not finish");
+                    break;
                 }
                 //----------------------------------if num 4  exit game need to finish it 
-                else if (true)
+                else if (MenuNumbre == 4)
                 {
                     System.Console.WriteLine("thank you for play");
                     break;
@@ -107,7 +127,7 @@ namespace task
                 else
                 {
                     System.Console.WriteLine("tape a numbre in a menue ");
-                   
+
 
                 }
 
@@ -132,6 +152,11 @@ namespace task
             int Numbrechoos = int.Parse(Console.ReadLine());
 
             return Numbrechoos;
+
+
+        }
+        static void checkBe()
+        {
 
 
         }
