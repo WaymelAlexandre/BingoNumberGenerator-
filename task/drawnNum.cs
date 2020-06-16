df4428e216c4a4a4891b0757da46521b93a8a3fa
alex w <103083020@student.swin.edu.au>
2020-06-16 16:27:33 +1000

    finish need to work on the extra question know

--- task/drawnNum.cs
+++ task/drawnNum.cs
@@ -14,13 +14,29 @@ namespace task
             this.nombre = Nombre;
         }
 
-        public int Diplaynumbre(int Nombre){
+        public int Diplaynumbre(int Nombre)
+        {
             Random r = new Random();
-            int RandomNum = r.Next(1, Nombre);
+            int RandomNum= 0;
+            
+            for (int i = 0; i < Nombre; i++)
+            {
+                
+                RandomNum = r.Next(1, Nombre);
+            }
+            
 
             return RandomNum;
         }
-        
+
+
+        /*Random rnd = new Random(); // <-- This line goes out of the loop        
+for (int i = 0; i < 20; i++) {
+    int temp = 0;
+    temp = rnd.Next(0, 9);
+    page[i] = temp;
+}*/
+
     }
 }
 
--- task/selection.cs
+++ task/selection.cs
@@ -14,6 +14,9 @@ namespace task
         {
             this.numbre = Numbre;
             int[] Myarray = new int[Numbre];
+            List<int> intList = new List<int>();
+
+
             while (true)
             {
 
@@ -25,9 +28,6 @@ namespace task
                 System.Console.WriteLine(" 4 Exit");
                 System.Console.WriteLine(" ");
                 System.Console.WriteLine(" ");
-
-
-
 
                 int Pickmenu = 0;
                 string PiMenu = System.Console.ReadLine();
@@ -41,43 +41,158 @@ namespace task
 
                     if (Pickmenu == 1)
                     {
-
-
-
-                        if (Myarray.Contains(TheNUMBRE))
+                        for (int i = 0; i < Numbre; i++)
                         {
-                            while (Myarray.Contains(TheNUMBRE) != Myarray.Contains(TheNUMBRE)  )
+
+
+                            if (!Myarray.Contains(TheNUMBRE))
                             {
-                                TheNUMBRE = Drawing.Diplaynumbre(numbre);
+                                System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                System.Console.WriteLine(TheNUMBRE);
+                                System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                Myarray[TheNUMBRE] = TheNUMBRE;
+                                intList.Add(TheNUMBRE);
+                                break;
+
                             }
+
+
+                            else if (Myarray.Contains(TheNUMBRE))
+
+                            {
+                                if (i == (Numbre - 5))
+                                {
+                                    System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                    System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                    System.Console.WriteLine("No more numbre \n the game should be finish for long time no give me a breack XD ");
+                                    System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                    System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                                    break;
+                                }
+
+                                TheNUMBRE = Drawing.Diplaynumbre(numbre);
+
+                            }
+
+
+
+                        }
+
+                    }
+
+
+
+                    else if (Pickmenu == 2)
+                    {
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        System.Console.WriteLine("Tape 1 to see the numbre by order of drawning. \nTape 2 to see the numbre print all numbers in numerical order.\n 3 for back to the menu");
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
+
+                        Pickmenu = 0;
+                        PiMenu = System.Console.ReadLine();
+                        result = int.TryParse(PiMenu, out Pickmenu);
+
+
+
+                        if (Pickmenu == 1)
+                        {
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("this is the list by drawning");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+
+
+                            for (int i = 0; i < intList.Count; i++)
+                            {
+                                Console.WriteLine(intList[i]);
+                            }
+                        }
+                        else if (Pickmenu == 2)
+                        {
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("This is the list by numerical order  ");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+
+                            // intList.Sort();
+                            // for (int i = 0; i < intList.Count ; i++)
+                            // {
+                            //     System.Console.WriteLine(intList[i]);
+
+                            // }
+
+                            for (int i = 0; i < Myarray.Length; i++)
+                            {
+                                if (Myarray[i] != 0)
+                                {
+                                    System.Console.WriteLine(Myarray[i]);
+                                }
+                            }
+                            //}
+                        }
+                        else if (Pickmenu == 3)
+                        {
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine(" ok let's back to the menu");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("pls choose what you want a do by ");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        }
+
+                        // for (int i = 0; i < Numbre; i++)
+                        // {
+                        //     System.Console.WriteLine(Myarray[i]);
+                        // }
+                    }
+
+                    else if (Pickmenu == 3)
+                    {
+                        System.Console.WriteLine("Please writ the numbre you want a check");
+                        int checkNUM = 0;
+                        string blabla = System.Console.ReadLine();
+                        bool result2 = int.TryParse(blabla, out checkNUM);
+
+                        if (intList.Contains(checkNUM))
+                        {
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("The numbre '" + checkNUM + "' is been already drawn");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
                             
                         }
                         else
                         {
-                            System.Console.WriteLine("--------------------------------------------");
-                            System.Console.WriteLine("--------------------------------------------");
-                            System.Console.WriteLine(TheNUMBRE);
-                            System.Console.WriteLine("--------------------------------------------");
-                            System.Console.WriteLine("--------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("This numbre  '" + checkNUM + "' it is still NOT been drawn ");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                            
                         }
-                        Myarray[TheNUMBRE] = TheNUMBRE;
-
-
-
 
                     }
-
-                    else if (Pickmenu == 2)
-                    {
-                        for (int i = 0; i < Numbre; i++)
-                        {
-                            System.Console.WriteLine(Myarray[i]);
-                        }
-                    }
-                    else if (Pickmenu == 3) { }
                     else if (Pickmenu == 4)
                     {
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
                         System.Console.WriteLine(" Thank you for play \n ");
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
+                        System.Console.WriteLine("--------------------------------------------------------------------------------------");
                         break;
                     }
                     else
@@ -87,28 +202,12 @@ namespace task
 
                     }
 
-
-
-
-
-
                 }
                 else
                 {
-                    System.Console.WriteLine("pls choose what you want a do by tapping \n 1 / 2 / 3 / 4 ");
+                    System.Console.WriteLine("pls choose what you want a do by tapping \n 1 for drwing numbre / 2 for list all the numbre be drawn / 3 check your numbre / 4 exit the game  ");
                 }
-                for (int i = 0; i < Numbre; i++)
-                {
-                    System.Console.WriteLine(Myarray[i]);
-                }
-
-
-
-
             }
         }
-
     }
-
-
 }
